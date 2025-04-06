707a8c66b1011a82d20d25be2c0382dbadfe272b
Sam Louis Walker-Penn <slwalkerpenn@gmail.com>
2025-04-06 16:08:11 +0100

    Remove vite-plugin-static-copy from configuration and clean up package dependencies

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,20 +1,8 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import { viteStaticCopy } from "vite-plugin-static-copy"
 import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "public/manifest.json",
-          dest: "",
-        },
-      ],
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
 })
